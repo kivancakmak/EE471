d63b010a7c487bd8ca523824256fe867c8554613
kivancakmak <kivanccakmak35@gmail.com>
2026-03-05 14:13:08 +0300

    added pleaseConformOnepass method

--- conform.py/conform.py
+++ conform.py/conform.py
@@ -1,1 +1,15 @@
 # This script is written by Kıvanç Çakmak
+
+
+
+def pleaseConformOnepass(caps):
+    start = 0
+    for i in range(1, len(caps)):
+        if caps[i] != caps[start]:
+            if i - start > 0:
+                print("People in positions", start, "through", i-1, "flip your caps!")
+            start = i
+
+    if caps[start] == 'F':
+        print("People in positions", start, "through", len(caps)-1, "flip your caps!")
+        
